4dc051b780e450664bb7849efe45f03f4121c42f
RJPGriffin <rowangee@gmail.com>
2017-10-25 22:07:34 +0100

    animated changin header

--- eChook/scripts/script.js
+++ eChook/scripts/script.js
@@ -1,3 +1,21 @@
-$("#title-header").headroom();
-
-
+function init() {
+    window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 50,
+            headerbar = document.querySelector("#nav-menu");
+            headerlogo = document.querySelector("#logo");
+        if (distanceY > shrinkOn) {
+            if (classie.has(headerlogo,"topLogo")) {
+                classie.remove(headerlogo,"topLogo");
+            }
+            if (classie.has(headerbar,"topNav")) {
+                classie.remove(headerbar,"topNav");
+            }
+        } else {
+            
+            classie.add(headerlogo,"topLogo");
+            classie.add(headerbar,"topNav");
+        }
+    });
+}
+window.onload = init();
