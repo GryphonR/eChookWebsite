6f17eae9d8a203c6c008ea50809684ed92371824
Rowan Griffin <rowangee@gmail.com>
2017-11-09 16:55:13 +0000

    added docs, grid to homescreen

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,6 @@
-var distanceY;
-
-function init() {
-    window.addEventListener('scroll', function(e){
-            distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            shrinkOn = 50,
-            headerbar = document.querySelector("#nav-menu");
-            headerlogo = document.querySelector("#logo");
-        if (distanceY > shrinkOn) {
-            if (classie.has(headerlogo,"topLogo")) {
-                classie.remove(headerlogo,"topLogo");
-            }
-            if (classie.has(headerbar,"topNav")) {
-                classie.remove(headerbar,"topNav");
-            }
-        } else if(windowsize > 900){
-
-            classie.add(headerlogo,"topLogo");
-            classie.add(headerbar,"topNav");
-        }
-    });
-}
-window.onload = init();
-
-var windowsize = $(window).width();
-
-$(window).resize(function() {
-  windowsize = $(window).width();
-  if(windowsize<900)
-  {
-    if (classie.has(headerlogo,"topLogo")) {
-        classie.remove(headerlogo,"topLogo");
-    }
-    if (classie.has(headerbar,"topNav")) {
-        classie.remove(headerbar,"topNav");
-    }
-  }
+$("body").on("click", ".nav a", function() {
+  $(".nav li").on("click", function() {
+    $(".nav").find(".active").removeClass("active");
+    $(this).parent().addClass("active");
+  });
 });
