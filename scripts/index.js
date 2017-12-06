4e985ca4a621bcc071d9542b2a6b71f046a9ace9
Rowan Griffin <rowangee@gmail.com>
2017-12-06 22:18:14 +0000

    Adding image slider

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('.slider-echookers').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-echookers-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+});
