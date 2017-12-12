d051f30b3e1b58d3c7eb3a15bb4058ff4e5ff64a
Rowan Griffin <rowangee@gmail.com>
2017-12-12 14:03:55 +0000

    Slick slider working

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,41 @@
 $(document).ready(function() {
   $('.slider-echookers').slick({
+    autoplay: true,
+    infinite: true,
+    centerMode: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-  });
-  $('.slider-echookers-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
     dots: true,
-    centerMode: true,
-    focusOnSelect: true
+    variableWidth: true,
+    adaptiveHeight: false,
+    responsive: [{
+      breakpoint: 500,
+      settings: {
+        dots: false,
+        arrows: false,
+        infinite: false,
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }]
   });
 });
+
+// $(document).ready(function() {
+//   console.log('Initialising Sliders');
+//   $('.slider-echookers').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: false,
+//     fade: true,
+//     asNavFor: '.slider-echookers-nav'
+//   });
+//   $('.slider-echookers-nav').slick({
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     asNavFor: '.slider-echookers',
+//     dots: true,
+//     centerMode: true,
+//     focusOnSelect: true
+//   });
+// });
