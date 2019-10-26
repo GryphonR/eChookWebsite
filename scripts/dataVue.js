bc46eb665b5917d6c29b699cc8c35a754fb986fb
Rowan Griffin <rowangee@gmail.com>
2019-10-26 18:42:35 +0100

    adding individual graphs

--- scripts/dataVue.js
+++ scripts/dataVue.js
@@ -2,16 +2,102 @@ var dataApp = new Vue({
   el: '#data-app',
   data: {
     active: true,
+    views: {
+      numerical: {
+        active: true
+      },
+      graph: {
+        active: true
+      },
+      map: {
+        active: true
+      }
+    },
     latest: {
-      VT: {
+      vTotal: {
         title: "Voltage Total",
         value: 24,
-        lowLimit: 22,
-        highLimit: 24,
-        max: 28,
+        low: 20,
+        high: 24,
+        max: 35,
+        min: 15,
+        unit: "Volts",
+        show: true
+      },
+      vUpper: {
+        title: "Upper Battery",
+        value: 12,
+        low: 10,
+        high: 14,
+        max: 20,
+        min: 0,
+        unit: "Volts",
+        show: true
+      },
+      vLower: {
+        title: "Lower Battery",
+        value: 12,
+        low: 10,
+        high: 14,
+        max: 20,
+        min: 0,
+        unit: "Volts",
+        show: true
+      },
+      current: {
+        title: "Current",
+        value: 20,
+        low: 0,
+        high: 30,
+        max: 35,
         min: 6,
-        unit: "Voltage"
-      }
+        unit: "Amps",
+        show: true
+      },
+      rpm: {
+        title: "Motor Speed",
+        value: 1800,
+        low: 0,
+        high: 2400,
+        max: 3000,
+        min: 1600,
+        unit: "RPM",
+        show: true
+      },
+      speed: {
+        title: "Motor Speed",
+        value: 20,
+        low: 0,
+        high: 50,
+        max: 50,
+        min: 0,
+        unit: "Mph",
+        show: true
+      },
+      throttle: {
+        title: "Throttle",
+        value: 100,
+        low: 0,
+        high: 101,
+        max: 100,
+        min: 0,
+        unit: "%",
+        show: true
+      },
+      tempOne: {
+        title: "Temperature 1",
+        value: 17,
+        low: 0,
+        high: 50,
+        max: 100,
+        min: 0,
+        unit: "°c",
+        show: true
+      },
+
     }
+  },
+  computed: {
+
   }
 })
